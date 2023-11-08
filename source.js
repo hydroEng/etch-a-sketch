c40993d4063cfc3c91425d089816e2f8643852fd
hydroEng <tahasoomro888@gmail.com>
2023-11-08 11:55:31 +1100

    Fix doubling border

--- source.js
+++ source.js
@@ -1,16 +1,30 @@
 const container = document.querySelector("#container")
 
+function styleCell(cell, i, j) {
+
+    cell.style.border = '1px solid'
+    // If cell is leftmost, keep left border. Similar for bottom cells.
+    if (i !=15) {
+        cell.style.borderBottomWidth = '0px'
+    }
+
+    if (j != 0) {
+        cell.style.borderLeftWidth = '0px'
+    }
+
+    cell.style.padding = '24px'
+    cell.style.boxSizing = 'border-box'
+
+    
+}
+
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div')
     row.style.display = "flex"
     for (let j = 0; j < 16; j++) {
         const cell = document.createElement('div')
-        cell.style.padding = '12px'
-        cell.style.borderColor = 'black'
-        cell.style.borderStyle = 'solid'
-        cell.style.border
+        styleCell(cell, i, j)
         row.appendChild(cell)
     }
     container.appendChild(row)
 }
-
