64199f0ff9b0387d019d12cca09af9a9d49c67b9
hydroEng <tahasoomro888@gmail.com>
2023-11-08 13:30:50 +1100

    Add button to change grid size via prompt()

--- source.js
+++ source.js
@@ -1,54 +1,70 @@
 const container = document.querySelector("#container")
+container.style.display = 'flex'
+container.style.flexDirection = 'column'
+container.style.width = '800px'
+container.style.height = '800px'
 
 const beige = 'rgb(230, 230, 220)'
-function styleCell(cell, i, j) {
+
+function styleCell(cell, i, j, size) {
     cell.style.border = '1px solid'
     // If cell is leftmost, keep left border. Similar for bottom cells.
-    if (i !=15) {
+    if (i != size-1) {
         cell.style.borderBottomWidth = '0px'
     }
 
     if (j != 0) {
         cell.style.borderLeftWidth = '0px'
     }
-
-    cell.style.padding = '24px'
-    cell.style.boxSizing = 'border-box'
     cell.style.backgroundColor = beige
-    console.log(cell.style)
+    cell.style.flex = '1'
 }
 
 function toggleOnHover(event) {
     green = 'rgb(101, 171, 124)'
-    red = 'rgb(210, 103, 118)'
-
     cell = event.target
 
     if (cell.style.backgroundColor == beige) {
         cell.style.backgroundColor = green
     }
-
-    else if (cell.style.backgroundColor == green) {
-        cell.style.backgroundColor = red
-    }
-
     else {
         cell.style.backgroundColor.backgroundColor = beige
     }
     
 }
 
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div')
-    row.style.display = "flex"
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div')
-        styleCell(cell, i, j)
-        cell.addEventListener(
-            'mouseover', toggleOnHover
-        )
-        row.appendChild(cell)
-    }
-    container.appendChild(row)
+function getGridDimensions() {
+    var input = prompt("Please enter grid size as an integer:")
+    num = Number(input)
+    setGrid(num)
 }
+
+function destroyGrid() {
+    container.innerHTML = ""
+    console.log(container.innerHTML)
+}
+function setGrid(size) {
+    destroyGrid()
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div')
+        row.style.display = "flex"
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement('div')
+            styleCell(cell, i, j, size)
+            cell.addEventListener(
+                'mouseover', toggleOnHover
+            )
+            row.appendChild(cell)
+        }
+        container.appendChild(row)
+    }    
+}
+
+// Initialize grid.
+
+setGrid(16)
+
+// Attach prompt to button.
+
+const button = document.querySelector("#new-grid")
+button.addEventListener('click', getGridDimensions)
